3f4643eb485a22af249f3b2d601896615f9802f6
jusjmkim <justinjmkim@gmail.com>
2014-09-13 19:51:04 -0400

    Basically works kind of i think

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ var express = require('express')
     , port = process.env.PORT || 8080
     , app = express()
     , mongo = require('mongodb')
-    , bodyParser = require('body-parser');
+    , bodyParser = require('body-parser')
+    , router = express.Router();
+
+var dataToSend = {};
 
 var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/handkeyjs';
 
@@ -13,90 +16,70 @@ var monk = require('monk')
     , collection = db.get('serialNumbers');
 
 // view engine setup
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 
-var server = http.createServer(app);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
+function parseToJson(key, value, res) {
+  dataToSend = {};
+  dataToSend[key] = value;
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
-app.get('/', function(req, res) {
-  res.on('data', function(data) {
-    check_computer_input(data, res);
-    setTimeout(function() {
-      res.json({});
-    }, 30000);
-  });
-});
+function randomNumberGenerator() {
+  var randomNumber = Math.random() * (1000 - 1) + 1;
+  return Math.floor(randomNumber);
+}
 
 function check_computer_input(data, res) {
-  var data_json = JSON.parse(data);
-  var randomNumber = randomNumber();
+  var randomNumber = randomNumberGenerator();
 
   // not first time computer connects
-  if (data_json.serial_number !== "undefined") {
-    parseToJson('computer_number', randomNumber, res);
+  if (!isNaN(data.serial_number)) {
+    res.json({'computer_number': randomNumber})
     collection.insert({
-      randomNumber: data_json.serial_number
+      computer_number: data.serial_number
     });
+
   // not first time computer connects
-  } else {
+  } else if (!isNaN(data.computer_number)) {
+    var computer_number = data.computer_number;
+    console.log(data);
+    console.log(computer_number);
     collection.find().success(function(computer_serials) {
-      parseToJson('serial_number', computer_serials[randomNumber], res); 
+      console.log(computer_serials);
+      computer_serials.forEach(function(computer_serial) {
+        if (computer_serial.computer_number === computer_number) {
+          parseToJson('serial_number', computer_number, res);
+          console.log(dataToSend);
+        }
+      });
+      res.json(dataToSend);
     });
   }
 }
 
-function parseToJson(key, value, res) {
-  var json_data = {key: value};
-  res.json(json_data);
-}
+router.use(function(req, res, next) {
+  next();
+});
 
-function randomNumber() {
-  return Math.random() * (1000 - 1) + 1;
-}
+router.get('/', function(req, res) {
+  res.json(dataToSend);
+});
 
-function listenToServer() {
-  server.listen(port);
-}
+router.route('/')
 
-(function() {
-  console.log('Server is running!');
-  listenToServer();
-})();
+  .post(function(req, res) {
+    var data = req.body;
+    check_computer_input(data, res);
+    setTimeout(function() {
+      dataToSend = {};
+    }, 30000);
+  });
+
+app.use('/serial_number', router);
+
+app.listen(port);
