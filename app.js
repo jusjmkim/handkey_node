fb43229de6a01197f153a6cf5c369055df3dc63f
jusjmkim <justinjmkim@gmail.com>
2014-09-13 14:11:58 -0400

    Testing posting random number

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongo = require('mongodb');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var mongoUri = proccess.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/handkeyjs';
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -22,8 +25,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+//  req.db = db;
+  next();
+});
+
 app.use('/', routes);
-app.use('/users', users);
+app.use('/serial_number', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,43 @@
 var express = require('express');
+var http = require('http');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+//  res.on('data', function(data) {
+    var dataString = randomNumber() // JSON.parse(data);
+    postPebble(dataString);
+ // }
 });
+
+function postPebble(data) {
+  var req = http.request(dataToHttp(data), function(res) {
+    console.log('The response' + res);
+  });
+
+  req.write(data);
+  req.end();
+}
+
+function dataToHttp(data){
+  return {
+    host: 'handkeyjs.herokuapp.com',
+    port: 3000,
+    path: '/serial_number',
+    method: 'POST',
+    headers: header(data)
+  };
+}
+
+function header(data) {
+  return {
+    'Content-Type': 'application/json',
+    'Content-Length': data.length
+  };
+}
+
+function randomNumber() {
+  return Math.random() * (1000 - 1) + 1;
+}
 
 module.exports = router;
